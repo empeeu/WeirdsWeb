77dc503febdac583b9976d889e6d15ab07405aa6
MP <empeeu@users.noreply.github.com>
2015-05-09 21:39:53 -0400

    Variable number of players available in the record page -- working on record page. Need to look into meteor templates to do this instead -- the basic javascript will not fly in, should use something a little higher level...

--- client/record.js
+++ client/record.js
@@ -1,15 +1,30 @@
 if (Meteor.isClient) {
-  Session.setDefault('record:nPlayers', 2)
+  Session.setDefault('Record:nPlayers', 2)
   Template.record.helpers({
     nPlayers: function(){
       return Session.get('Record:nPlayers');
     } 
   });
   Template.record.events({
-    "change #n-players" : function(event) {
+    "change #NPlayers" : function(event) {
       var nPlayers = event.target.value;
-      
-      Session.set('Record:nPlayers', nPlayers);
+      Session.set('Record:nPlayers', nPlayers)
+      var container = document.getElementById("PlayerId")
+      // Clear the previous contents of the container
+      while (container.hasChildNodes()){
+        container.removeChild(container.lastChild);
+      }
+      for (i=0; i < nPlayers; i++){
+        // Append a node with a random text
+        container.appendChild(document.createTextNode("Player " + (i+1)));
+        // Create an <input> element, set its type and name attributes
+        var input = document.createElement("input");
+        input.type = "text";
+        input.name = "member" + i;
+        container.appendChild(input);
+        // Append a line break 
+        container.appendChild(document.createElement("br"));
+      }
     }
   })
 }

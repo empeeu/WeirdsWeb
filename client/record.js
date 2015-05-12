0984b99aec54706b8acbca77ccb22522330f3795
MP <empeeu@users.noreply.github.com>
2015-05-11 21:35:25 -0400

    Now using meteor templates to loop over players instead of raw javascript -- much nicer

--- client/record.js
+++ client/record.js
@@ -1,30 +1,19 @@
 if (Meteor.isClient) {
+  var Player = [];
   Session.setDefault('Record:nPlayers', 2)
   Template.record.helpers({
     nPlayers: function(){
       return Session.get('Record:nPlayers');
+    },
+    Players : function() {
+      var nPlayers = Session.get('Record:nPlayers');
+      return _.range(nPlayers)
     } 
   });
   Template.record.events({
     "change #NPlayers" : function(event) {
       var nPlayers = event.target.value;
       Session.set('Record:nPlayers', nPlayers)
-      var container = document.getElementById("PlayerId")
-      // Clear the previous contents of the container
-      while (container.hasChildNodes()){
-        container.removeChild(container.lastChild);
-      }
-      for (i=0; i < nPlayers; i++){
-        // Append a node with a random text
-        container.appendChild(document.createTextNode("Player " + (i+1)));
-        // Create an <input> element, set its type and name attributes
-        var input = document.createElement("input");
-        input.type = "text";
-        input.name = "member" + i;
-        container.appendChild(input);
-        // Append a line break 
-        container.appendChild(document.createElement("br"));
-      }
     }
   })
 }
